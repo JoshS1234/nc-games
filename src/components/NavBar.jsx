907e8b01a2d94b5e439d279e9e184b707b447f08
josh <joshua.spence@hotmail.co.uk>
2022-09-26 13:56:30 +0100

    added a nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 const NavBar = () => {
-  return (<h1>Nav bar</h1>
-  <ol>
-  </ol>);
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/review-list">Reviews</Link>
+      <Link to="/user-list">Users</Link>
+    </nav>
+  );
 };
 
 export default NavBar;
